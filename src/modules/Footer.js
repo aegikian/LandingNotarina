d1e22c9f10fc50113cb657e9abc38dc073cccce3
Odinson96 <egk090@gmail.com>
2023-06-03 21:30:40 +0400

    Update Footer.js

--- src/modules/Footer.js
+++ src/modules/Footer.js
@@ -4,7 +4,7 @@ import '../css/Contacts.css';
 const Footer = () => {
     return (
         <div className='Footer'>
-            <h5>Все права защищены © 2020 НОТАРИНА</h5>
+            <h5>Все права защищены © 2023 НОТАРИНА</h5>
         </div>
     )
 }
